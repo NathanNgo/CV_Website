dac7b831839b68eab5db782f2ea6b9c8512a80e5
Nathan Ngo <nathanj238@gmail.com>
2023-12-09 00:54:15 +1100

    Added basic layout

---
+++ src/PageContent.js
@@ -0,0 +1,11 @@
+import styles from "./PageContent.module.css"
+
+function PageContent() {
+    return (
+        <div>
+            <h1>NATHAN NGO</h1>
+        </div>
+    )
+}
+
+export default PageContent
